e75e551248746a23dd7e96956229e7489df10463
unknown <magsimsharafa@gmail.com>
2021-12-08 11:37:20 +0200

    finishing sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,16 +12,18 @@ const config = {
     measurementId: "G-VS2112S7Y3"
   };
 
+  firebase.initializeApp(config);
+
   export const createUserProfileDocument = async (userAuth, additionalData)=>{
       if(!userAuth)return;
 
-      const userRef = firestore.doc(`users/${userAuth.uid}`)
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-      const snapShot = await userRef.get()
+      const snapShot = await userRef.get();
 
-      if(!snapShot.exist){
-          const {displayName, email}=userAuth;
-          const createdAt=new Date();
+      if(!snapShot.exists){
+          const {displayName, email} = userAuth;
+          const createdAt = new Date();
 
           try{
             await userRef.set({
@@ -36,9 +38,8 @@ const config = {
       }
 
       return userRef;
-  }
+  };
 
-  firebase.initializeApp(config);
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
